656cb64cdc3704be4fb3b647866b36f6a9928dec
pavani <pavani.n@egradtutor.in>
2025-04-04 14:49:24 +0530

    adding css folder

--- frontend_iitjee/src/Components/LandingPagesFolder/LandingPageIITJEE.jsx
+++ frontend_iitjee/src/Components/LandingPagesFolder/LandingPageIITJEE.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import "../../Styles/LandingPageCSS/LandingPageIITJEE.module.css";
+import LandingPageNavbar from './LandingPageNavbar';
 
 export default function LandingPageIITJEE() {
   return (
     <div>
-      hello
+      <LandingPageNavbar/>
     </div>
   )
 }
