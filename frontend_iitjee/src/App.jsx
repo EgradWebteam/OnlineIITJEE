51cd2ddf83e01e659bbd6412b686f7d84087efc9
AHarshitha04 <harshitha.a@egradtutor.in>
2025-04-04 18:04:10 +0530

    StudentRegistration form

--- frontend_iitjee/src/App.jsx
+++ frontend_iitjee/src/App.jsx
@@ -1,6 +1,7 @@
 import React, {Suspense} from 'react'
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-const LandingPageIITJEE = React.lazy(() => import('./Components/LandingPagesFolder/LandingPageIITJEE'));
+const LandingPageIITJEE = React.lazy(() => import('./Components/LandingPagesFolder/LandingPageIITJEE.jsx'));
+const StudentRegistration = React.lazy(() => import('./Components/StudentDashboardPagesFolder/StudentLoginpages/StudentRegistration.jsx'));
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<LandingPageIITJEE />} />
+      <Route path="/StudentRegistrationPage" element={<StudentRegistration />} />
     </Routes>
     </BrowserRouter>
    </Suspense>
