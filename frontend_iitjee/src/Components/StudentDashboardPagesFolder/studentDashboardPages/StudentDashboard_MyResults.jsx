2e0be9f06eb0a284c7b925cc83615b6fb70930d0
pavani <pavani.n@egradtutor.in>
2025-04-15 10:31:22 +0530

    student dashboard my results ui

--- frontend_iitjee/src/Components/StudentDashboardPagesFolder/studentDashboardPages/StudentDashboard_MyResults.jsx
+++ frontend_iitjee/src/Components/StudentDashboardPagesFolder/studentDashboardPages/StudentDashboard_MyResults.jsx
@@ -1,12 +1,27 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
+import globalCSS from "../../../Styles/Global.module.css";
+import styles from "../../../Styles/StudentDashboardCSS/StudentDashboard.module.css";
+import { MdKeyboardDoubleArrowRight } from "react-icons/md";
 
 export default function StudentDashboard_MyResults() {
      useEffect(() => {
     console.log("myresults")
       },[])
   return (
-    <div>
-      StudentDashboard_MyResults
+    <div className={styles.StudentDashboardMyCoursesMainDiv}>
+      <div className={globalCSS.stuentDashboardGlobalHeading}>
+        <h3>My Results</h3>
+      </div>
+      <div className={styles.myResultsSubContainer}>
+        <div className={styles.resultsMainDiv}>
+          <div className={styles.resultsSubDiv}>
+            <p className={styles.testNameInResults}>TEST</p>
+            <div className={styles.resultsViewReportBtn}>
+              <button>VIEW REPORT <span className={styles.resultsBtnIcon}><MdKeyboardDoubleArrowRight /></span> </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
