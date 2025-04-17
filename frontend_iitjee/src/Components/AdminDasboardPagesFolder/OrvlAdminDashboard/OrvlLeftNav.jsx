1793f8f47946e55f43ccc07afac25b99956785cd
chinnari-2002 <chinnari.a@egradtutor.in>
2025-04-17 12:13:47 +0530

    CHANGES IN ORVL

--- frontend_iitjee/src/Components/AdminDasboardPagesFolder/OrvlAdminDashboard/OrvlLeftNav.jsx
+++ frontend_iitjee/src/Components/AdminDasboardPagesFolder/OrvlAdminDashboard/OrvlLeftNav.jsx
@@ -15,14 +15,14 @@ const  OrvlLeftNav = ({ onMenuClick, activeComponent }) => {
           onClick={() => onMenuClick('dashboard')}
         >
           <TbDeviceDesktopAnalytics className={styles.icon} />
-          orvl Dashboard
+          ORVL Dashboard 
         </li>
         <li
           className={activeComponent === 'course-creation' ? styles.active : ''}
           onClick={() => onMenuClick('course-creation')}
         >
           <FaTableList className={styles.icon} />
-           orvl Course Creation
+          ORVL Course Creation
         </li>
       
         <li
@@ -30,7 +30,7 @@ const  OrvlLeftNav = ({ onMenuClick, activeComponent }) => {
           onClick={() => onMenuClick('topic-creation')}
         >
           <DiGhostSmall className={styles.icon} />
-           orvl Topic Creation
+          ORVL Topic Creation
         </li>
       
       </ul>
