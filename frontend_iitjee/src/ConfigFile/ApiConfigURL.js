7834890fb57b097197489ec76c96d1fd7cdae129
chinnari-2002 <chinnari.a@egradtutor.in>
2025-04-24 13:35:38 +0530

    css changes in popup

--- frontend_iitjee/src/ConfigFile/ApiConfigURL.js
+++ frontend_iitjee/src/ConfigFile/ApiConfigURL.js
@@ -1,12 +1,12 @@
 
-// export  const BASE_URL = `http://localhost:5000`;
-// export const frontEndURL=`http://localhost:${window.location.port}`;
+export  const BASE_URL = `http://localhost:5000`;
+export const frontEndURL=`http://localhost:${window.location.port}`;
 
 
 
-export const frontEndURL = `https://wonderful-island-062abce00.6.azurestaticapps.net`
+// export const frontEndURL = `https://wonderful-island-062abce00.6.azurestaticapps.net`
 
 
  
-export const BASE_URL = `https://iit-server-fyahe4drftaadxgq.centralindia-01.azurewebsites.net`
+// export const BASE_URL = `https://iit-server-fyahe4drftaadxgq.centralindia-01.azurewebsites.net`
  
