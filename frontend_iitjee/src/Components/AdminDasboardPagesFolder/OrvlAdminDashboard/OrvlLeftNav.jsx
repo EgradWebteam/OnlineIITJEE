0726a8bb529c927985a4e24323a2373d185a398f
chinnari-2002 <chinnari.a@egradtutor.in>
2025-04-30 10:30:29 +0530

    responsive in the admindashboard

--- frontend_iitjee/src/Components/AdminDasboardPagesFolder/OrvlAdminDashboard/OrvlLeftNav.jsx
+++ frontend_iitjee/src/Components/AdminDasboardPagesFolder/OrvlAdminDashboard/OrvlLeftNav.jsx
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../../../Styles/AdminDashboardCSS/AdminDashboard.module.css'; 
 import { TbDeviceDesktopAnalytics } from 'react-icons/tb';  
 import { FaTableList } from 'react-icons/fa6'; 
-import { DiGhostSmall } from 'react-icons/di'; 
+import { DiGhostSmall } from 'react-icons/di';
+import { FaBars, FaTimes } from 'react-icons/fa'; 
 
 
 
 const  OrvlLeftNav = ({ onMenuClick, activeComponent }) => {
+    const [orvlLeftBarMenu,setOrvlLeftbarMenu] = useState()
+   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+const handleResize = () => {
+    setIsMobile(window.innerWidth <= 768);
+    if (window.innerWidth > 768) {
+      setOrvlLeftbarMenu(true); // Always open sidebar in desktop
+    } else {
+      setOrvlLeftbarMenu(false); // Hide sidebar by default in mobile
+    }
+  };
+
+  useEffect(() => {
+    handleResize(); // Run once on mount
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  const orvlLeftBarToggleMenu=()=>{
+    setOrvlLeftbarMenu(prev => !prev);
+  }
+
   return (
-    <div className={styles.sidebar}>
+    <div>
+       <div className={styles.HamburgerHeader} onClick={orvlLeftBarToggleMenu}>
+              {orvlLeftBarMenu ? <FaTimes/> : <FaBars />}
+             </div>
+             {orvlLeftBarMenu && (
+    <div className={`${styles.sidebar} ${orvlLeftBarMenu ? styles.sidebarOpen :''}`}>
       <ul className={styles.sidebarMenu}>
         <li
           className={activeComponent === 'dashboard' ? styles.active : ''}
@@ -34,6 +60,8 @@ const  OrvlLeftNav = ({ onMenuClick, activeComponent }) => {
         </li>
       
       </ul>
+    </div>
+    )}
     </div>
   );
 };
--- frontend_iitjee/src/ConfigFile/ApiConfigURL.js
+++ frontend_iitjee/src/ConfigFile/ApiConfigURL.js
@@ -1,12 +1,9 @@
 
-// export  const BASE_URL = `http://localhost:5000`;
-// export const frontEndURL=`http://localhost:${window.location.port}`;
+export  const BASE_URL = `http://localhost:5000`;
+export const frontEndURL=`http://localhost:${window.location.port}`;
 
 
 
-export const frontEndURL = `https://iitjee.egradtutor.in`
-
-
- 
-export const BASE_URL = `https://iit-server-fyahe4drftaadxgq.centralindia-01.azurewebsites.net`
+// export const frontEndURL = `https://iitjee.egradtutor.in`
+// export const BASE_URL = `https://iit-server-fyahe4drftaadxgq.centralindia-01.azurewebsites.net`
  
