02833cda9930f8007399666014b16a8d536592b1
chinnari-2002 <chinnari.a@egradtutor.in>
2025-05-05 15:44:26 +0530

    changes in exercises

--- frontend_iitjee/src/Components/StudentDashboardFilesFolder/StudentDashboardFiles/StudentDashboard_MyCourses.jsx
+++ frontend_iitjee/src/Components/StudentDashboardFilesFolder/StudentDashboardFiles/StudentDashboard_MyCourses.jsx
@@ -136,8 +136,6 @@ export default function StudentDashboard_MyCourses({ studentId,userData }) {
     )}
   </div>
 )}
-
-
       {showQuizContainer && (
         <div className={styles.studentDashboardMyCoursesMainDiv}>
           <div className={globalCSS.stuentDashboardGlobalHeading}>
