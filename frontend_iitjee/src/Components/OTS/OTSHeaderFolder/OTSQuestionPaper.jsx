686f3f375d54de872916f032839f1f5f498f19b7
shravya <shravya.t@egradtutor.in>
2025-05-20 14:56:35 +0530

    Question id Change in View Questions

--- frontend_iitjee/src/Components/OTS/OTSHeaderFolder/OTSQuestionPaper.jsx
+++ frontend_iitjee/src/Components/OTS/OTSHeaderFolder/OTSQuestionPaper.jsx
@@ -23,61 +23,80 @@ const OTSQuestionPaper = ({ testName,realTestId, closeQuestionPaper }) => {
   }, []);
 
 
-  return (
-    <div className={styles.OTSQuestionPaperMainDiv}>
-      <div className={styles.OTSQuestionPaperSubDiv}>
-        {/* Header */}
-        <div className={styles.navDivForQuestionPaper}>
-          <div className={styles.logoDiv}>
-            <img src={egradLogo} alt='logo' />
-          </div>
-          <div className={styles.testNameInQuestionPaper}>
-            <h3>{testName}</h3>
-          </div>
-          <div className={styles.OTSQuestionPapercloseBtn}>
-            <button onClick={closeQuestionPaper}>Close</button>
-          </div>
+return (
+  <div className={styles.OTSQuestionPaperMainDiv}>
+    <div className={styles.OTSQuestionPaperSubDiv}>
+      {/* Header */}
+      <div className={styles.navDivForQuestionPaper}>
+        <div className={styles.logoDiv}>
+          <img src={egradLogo} alt="logo" />
         </div>
+        <div className={styles.testNameInQuestionPaper}>
+          <h3>{testName}</h3>
+        </div>
+        <div className={styles.OTSQuestionPapercloseBtn}>
+          <button onClick={closeQuestionPaper}>Close</button>
+        </div>
+      </div>
 
-        {/* Questions */}
-        <div>
- 
-      {questionData.subjects?.map((subject) => (
-        <div key={subject.subjectId} >
-          {subject.sections.map((section) => (
-            <div key={section.sectionId}>
-              {section.questions.map((question) => (
-                <div key={question.question_id}  className={styles.OTSQuestionContainer}>
-                  <p className={styles.OTSQuestionPaperNumber}>Question No: {question.question_id}</p>
-                  <div className={styles.OTSQuestionPaperQuestionImgDIv}>
-                  <img
-                    src={question.questionImgName}
-                    alt={`Question ${question.question_id,question.questionImgName}`} 
-                  />
-                  </div>
-                  <div style={{ marginTop: "1rem" }}>
-                    {question.options.map((option) => (
-                      <div key={option.option_id} style={{ display: "flex", alignItems: "center" }}>
-                        <strong className={styles.OTSQuestionPaperOptionIndex}>({option.option_index})</strong>
-                        <div className={styles.OTSQuestionPaperOptionsDiv}>
-                        <img
-                          src={option.optionImgName}
-                          alt={`Option ${option.option_index}`}
-                        />
+      {/* Questions */}
+      <div>
+        {(() => {
+          let questionCounter = 1; // Declare the counter here
+
+          return questionData.subjects?.map((subject) => (
+            <div key={subject.subjectId}>
+              {subject.sections.map((section) => (
+                <div key={section.sectionId}>
+                  {section.questions.map((question) => {
+                    const currentQuestionNumber = questionCounter++; // use & increment counter here
+                    return (
+                      <div
+                        key={question.question_id}
+                        className={styles.OTSQuestionContainer}
+                      >
+                        <p className={styles.OTSQuestionPaperNumber}>
+                          Question No: {currentQuestionNumber}
+                        </p>
+                        <div className={styles.OTSQuestionPaperQuestionImgDIv}>
+                          <img
+                            src={question.questionImgName}
+                            alt={`Question ${currentQuestionNumber}`}
+                          />
+                        </div>
+                        <div style={{ marginTop: "1rem" }}>
+                          {question.options.map((option) => (
+                            <div
+                              key={option.option_id}
+                              style={{ display: "flex", alignItems: "center" }}
+                            >
+                              <strong
+                                className={styles.OTSQuestionPaperOptionIndex}
+                              >
+                                ({option.option_index})
+                              </strong>
+                              <div className={styles.OTSQuestionPaperOptionsDiv}>
+                                <img
+                                  src={option.optionImgName}
+                                  alt={`Option ${option.option_index}`}
+                                />
+                              </div>
+                            </div>
+                          ))}
                         </div>
                       </div>
-                    ))}
-                  </div>
+                    );
+                  })}
                 </div>
               ))}
             </div>
-          ))}
-        </div>
-      ))}
-    </div>
+          ));
+        })()}
       </div>
     </div>
-  );
+  </div>
+);
+
 };
 
 export default OTSQuestionPaper;
